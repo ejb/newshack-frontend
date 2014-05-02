a72b6b49a2d812c2ad16bd90b950c7f78cdeb606
bentleye <elliot.bentley@wsj.com>
2014-05-02 13:12:19 +0100

    Include dist folder

---
+++ dist/proxy.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: application/json');
+
+    $url = 'http://ec2-54-72-215-27.eu-west-1.compute.amazonaws.com/api/public/random/';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // curl_setopt($ch, CURLOPT_PROXY, 'http://proxy.dowjones.net:80');
+    $ukr_data_raw = curl_exec($ch);
+    curl_close($ch);
+    echo $ukr_data_raw;        
+    ?>
