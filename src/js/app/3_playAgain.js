e4a0435f9accca9e049e8e6904577f8bfa2a1dfa
bentleye <elliot.bentley@wsj.com>
2014-05-02 14:52:44 +0100

    Try again

--- src/js/app/3_playAgain.js
+++ src/js/app/3_playAgain.js
@@ -25,21 +25,26 @@ function playAgain(){
 function finishGame(){
 	var msg = "You got <b>"+score+" out of "+current+"</b> answers correct.";
 	if (score === current) {
-		msg += " You sure know your news."
+		msg += " You sure know your news. <span class='try-again'>Try again</span>, "
 	} else if (score === current-1) {
-		msg += " You're pretty up to date with news, I guess."
+		msg += " You're pretty up to date with news, I guess. <span class='try-again'>Try again</span>?"
 	} else {
-		msg += " Maybe you should read the news a bit more often."
+		msg += " <span class='try-again'>Try again</span>, and this time, think tenuous!"
 	}
 	
 	tweet = "I got "+score+" out of "+current+" answers correct in the tenuous connections news quiz. Can you do better?";
 	tweet =  encodeURIComponent(tweet);
 	
-   var tbutton = '<p>Share your score</p><a href="https://twitter.com/share?text='+tweet+'" class="twitter-share-button" data-lang="en">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
+   var tbutton = '<p>Share your score</p><a href="https://twitter.com/share?text='+tweet+'" class="twitter-share-button" data-lang="en">Tweet</span><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
 
 
 
 	$('.final-score').html( msg + tbutton );
 	
 	$('.final-score').show();	
+	
+	
+	$('.try-again').click(function(){
+		window.location.reload();
+	})
 }
